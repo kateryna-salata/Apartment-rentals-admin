67395e80ad070ac9dac4569c7ed85e9404f3916e
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-11-05 20:04:34 +0000

    fixed delete button

--- Apartment-Rentals/src/components/ListItem.jsx
+++ Apartment-Rentals/src/components/ListItem.jsx
@@ -1,23 +1,25 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 export const ListItem = ({ item, data, setData }) => {
   function handleDelete(id) {
     const filteredData = data.filter((i) => i.id !== id);
     setData(filteredData);
   }
-  const isSuperHost = item.host_is_superhost
+  const isSuperHost = item.host_is_superhost;
   return (
     <div>
-      <Link to={`/details/${item.id}`} key={item.id}>
-      <div className="card" >
+      <div className="card" key={item.id}>
         <img src={item.picture_url} alt="" />
         <div className="text">
-          <h2>{item.name}  {isSuperHost ? <div className="super">⭐</div> : null}</h2>
+          <Link to={`/details/${item.id}`}>
+            <h2>
+              {item.name} {isSuperHost ? <div className="super">⭐</div> : null}
+            </h2>
+          </Link>
           <p>{item.host_location}</p>
-         
+
           <button onClick={() => handleDelete(item.id)}>Delete</button>
         </div>
       </div>
-      </Link>
     </div>
   );
 };
