48394c5e3f0121b9f2fac6d18621760fb18d6a0e
Franziska Scheer <franzi-scheer@web.de>
2024-11-14 21:31:04 +0100

    add favourites

--- Apartment-Rentals/src/components/ListItem.jsx
+++ Apartment-Rentals/src/components/ListItem.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 export const ListItem = ({ item, data, setData }) => {
   function handleDelete(id) {
     const filteredData = data.filter((i) => i.id !== id);
     setData(filteredData);
   }
+
   const isSuperHost = item.host_is_superhost;
+
+  const handleFav = (id) => {
+    const updatedData = data.map((e) => (e.id === id ? { ...e, isFav: !e.isFav } : e));
+    setData(updatedData); // Update the state to reflect the change
+  };
+
   return (
     <>
-      <div className="card" key={item.id}>
+      <div className={`card ${item.isFav ? "isFav" : ""}`} key={item.id}>
         <img src={item.picture_url} alt="" />
         <div className="text">
           <Link to={`/details/${item.id}`}>
@@ -19,6 +28,9 @@ export const ListItem = ({ item, data, setData }) => {
           <p>{item.price}</p>
           <button className="btn delete-btn" onClick={() => handleDelete(item.id)}>
             Delete
+          </button>
+          <button className="fav-btn" onClick={() => handleFav(item.id)}>
+            {item.isFav ? "❤️" : "🤍"}
           </button>
         </div>
       </div>
